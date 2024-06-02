adf421f3fd3a2b9d991337e80dbfc299cf7e4aa0
Anil Kumar <anilkumarpulla890@gmail.com>
2024-06-02 07:17:32 +0530

    First commit

---
+++ src/main/java/com/gap/FeedsSkuApplication.java
@@ -0,0 +1,13 @@
+package com.gap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedsSkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedsSkuApplication.class, args);
+	}
+
+}
